85bf1aff2103f926c1517e31813003df8b8d34d8
Vincent <phenolplus@hotmail.com>
2011-10-20 10:15:52 +0800

    some thing...

---
+++ src/test_hello/proj/Test_helloActivity.java
@@ -0,0 +1,13 @@
+package test_hello.proj;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test_helloActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
